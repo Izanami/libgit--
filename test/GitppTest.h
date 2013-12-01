5583e8ae18b4a7e2327f96b4d8c2a964bb22dbd6
Adrien Jeser <adrien@jeser.me>
2013-12-01 08:57:28 +0100

    Git::init + Test

--- test/GitppTest.h
+++ test/GitppTest.h
@@ -2,16 +2,21 @@
 
 #define GITPP_TEST
 #include <cppunit/extensions/HelperMacros.h>
+#include "boost/filesystem.hpp"
 
 class GitppTest:public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(GitppTest);
+    CPPUNIT_TEST(testInit);
     CPPUNIT_TEST(testConstructor);
     CPPUNIT_TEST_SUITE_END();
-
+    private:
+    boost::filesystem::path tmpdir;
   public:
     void setUp();
     void tearDown();
+    void testInit();
     void testConstructor();
+
 };
 
 #endif                          // GITPP_TEST
