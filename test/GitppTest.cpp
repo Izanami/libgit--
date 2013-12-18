9a788465436867ecd98ad1867fdaa8cb456fde0d
Adrien Jeser <adrien@jeser.me>
2013-12-18 22:16:36 +0100

    Conversion de char vers string

--- test/GitppTest.cpp
+++ test/GitppTest.cpp
@@ -10,7 +10,7 @@ void GitppTest::setUp()
 {
     path = unique_path();
     create_directories(path);
-    repo = Git::Repo::init(path.string().c_str());
+    repo = Git::Repo::init(path.string());
 }
 
 void GitppTest::tearDown()
@@ -20,18 +20,18 @@ void GitppTest::tearDown()
 
 void GitppTest::testInit()
 {
-    Git::Repo::init(path.string().c_str());
+    Git::Repo::init(path.string());
 }
 
 void GitppTest::testConstructor()
 {
-    Git::Repo(path.string().c_str());
+    Git::Repo(path.string());
 }
 
 void GitppTest::testClone()
 {
     boost::filesystem::path tmpdir = unique_path();
-    repo->clone(tmpdir.string().c_str());
+    repo->clone(tmpdir.string());
     remove_all(tmpdir);
 }
 
