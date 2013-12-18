87aabc4e5977f5ebafa9622340d401a113b04f05
Adrien Jeser <adrien@jeser.me>
2013-12-18 22:16:36 +0100

    Conversion de char vers string

--- src/repo.cpp
+++ src/repo.cpp
@@ -1,9 +1,9 @@
 #include "repo.h"
 
 namespace Git {
-    Repo::Repo(const char *path) {
+    Repo::Repo(const std::string &path) {
         git_threads_init();
-        int error = git_repository_open(&repo, path);
+        int error = git_repository_open(&repo, path.c_str());
          exception(error);
     }
 
@@ -29,41 +29,41 @@ namespace Git {
     }
 
 
-    std::shared_ptr <Repo> Repo::init(const char *path, const bool bare) {
+    std::shared_ptr <Repo> Repo::init(const std::string &path, const bool bare) {
         git_repository *repo = nullptr;
-        int error = git_repository_init(&repo, path, bare);
+        int error = git_repository_init(&repo, path.c_str(), bare);
         exception(error);
         return std::shared_ptr <Repo> (new Repo(repo));
     }
 
-    Repo *Repo::clone(const char *path) {
+    Repo *Repo::clone(const std::string &path) {
         git_repository *repolone = nullptr;
-        int error = git_clone(&repolone, this->path(), path, NULL);
+        int error = git_clone(&repolone, this->path().c_str(), path.c_str(), NULL);
         exception(error);
         return new Repo(repolone);
     }
 
-    const char *Repo::path() {
+    const std::string Repo::path() {
         return git_repository_path(repo);
     }
 
-    void Repo::lookup(const char *commit) {
+    void Repo::lookup(const std::string &commit) {
         git_commit *commit_c;
-        git_oid commit_oid = oid(commit);
+        git_oid commit_oid = oid(commit.c_str());
         int error = git_commit_lookup(&commit_c, repo, &commit_oid);
         exception(error);
     }
 
-    git_oid Repo::oid(const char *sha) {
+    git_oid Repo::oid(const std::string &sha) {
         git_oid oid;
-        int error = git_oid_fromstr(&oid, sha);
+        int error = git_oid_fromstr(&oid, sha.c_str());
         exception(error);
         return oid;
     }
-    const char *Repo::oid(git_oid & oid) {
-        char *shortsha = new char[10];
+    const std::string Repo::oid(git_oid & oid) {
+        char shortsha[10];
         git_oid_tostr(shortsha, 9, &oid);
-        return shortsha;
+        return std::string(shortsha);
     }
 
 }                               // Git
