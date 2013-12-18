bdfb75e26fc01ad0a0d3b4e0cfe92733bca6a425
Adrien Jeser <adrien@jeser.me>
2013-12-18 22:16:36 +0100

    Conversion de char vers string

--- src/repo.h
+++ src/repo.h
@@ -7,27 +7,27 @@
 namespace Git {
     class Repo {
       public:
-        Repo(const char *path);
+        Repo(const std::string &path);
          Repo(git_repository * repo);
          ~Repo();
-        static std::shared_ptr <Repo> init(const char *path, const bool bare = false);
+        static std::shared_ptr <Repo> init(const std::string &path, const bool bare = false);
 
         // Clone
-        Repo *clone(const char *path);
-        static void clone(const char *url, const char *path);
+        Repo *clone(const std::string &path);
+        static void clone(const std::string &url, const std::string &path);
 
         // Lookup
-        void lookup(const char *commit);
+        void lookup(const std::string &commit);
 
         // Getter
-        const char *path();
+        const std::string path();
 
       private:
         Repo(Repo &Repo);
          git_repository * repo;
         static void exception(int error);
-        static git_oid oid(const char *sha);
-        static const char *oid(git_oid & oid);
+        static git_oid oid(const std::string &sha);
+        static const std::string oid(git_oid & oid);
     };
 }
 #endif
