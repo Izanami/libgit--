422cb3b12d8f8fa534cd6d0d45bf6b75e8af8cbb
Adrien Jeser <adrien@jeser.me>
2013-12-19 07:33:12 +0100

    modifié :   LICENSE

--- src/repo.cpp
+++ src/repo.cpp
@@ -1,13 +1,11 @@
 #include "repo.h"
 
 namespace Git {
-    Repo::Repo(const std::string &path) {
+    Repo::Repo(const std::string & path) {
         git_threads_init();
         int error = git_repository_open(&repo, path.c_str());
          exception(error);
-    }
-
-    Repo::Repo(git_repository * repo) {
+    } Repo::Repo(git_repository * repo) {
         this->repo = repo;
     }
 
@@ -29,24 +27,26 @@ namespace Git {
     }
 
 
-    std::shared_ptr <Repo> Repo::open(const std::string &path) {
-        return std::shared_ptr <Repo> (new Repo(path));
+    std::shared_ptr < Repo > Repo::open(const std::string & path) {
+        return std::shared_ptr < Repo > (new Repo(path));
     }
 
-    std::shared_ptr <Repo> Repo::open(git_repository * repo) {
-        return std::shared_ptr <Repo> (new Repo(repo));
+    std::shared_ptr < Repo > Repo::open(git_repository * repo) {
+        return std::shared_ptr < Repo > (new Repo(repo));
     }
 
-    std::shared_ptr <Repo> Repo::init(const std::string &path, const bool bare) {
+    std::shared_ptr < Repo > Repo::init(const std::string & path,
+                                        const bool bare) {
         git_repository *repo = nullptr;
         int error = git_repository_init(&repo, path.c_str(), bare);
         exception(error);
         return open(repo);
     }
 
-    std::shared_ptr <Repo> Repo::clone(const std::string &path) {
+    std::shared_ptr < Repo > Repo::clone(const std::string & path) {
         git_repository *repolone = nullptr;
-        int error = git_clone(&repolone, this->path().c_str(), path.c_str(), NULL);
+        int error =
+            git_clone(&repolone, this->path().c_str(), path.c_str(), NULL);
         exception(error);
         return open(repolone);
     }
@@ -55,14 +55,14 @@ namespace Git {
         return git_repository_path(repo);
     }
 
-    void Repo::lookup(const std::string &commit) {
+    void Repo::lookup(const std::string & commit) {
         git_commit *commit_c;
         git_oid commit_oid = oid(commit.c_str());
         int error = git_commit_lookup(&commit_c, repo, &commit_oid);
         exception(error);
     }
 
-    git_oid Repo::oid(const std::string &sha) {
+    git_oid Repo::oid(const std::string & sha) {
         git_oid oid;
         int error = git_oid_fromstr(&oid, sha.c_str());
         exception(error);
--- src/repo.h
+++ src/repo.h
@@ -7,28 +7,30 @@
 namespace Git {
     class Repo {
       public:
-         ~Repo();
-        static std::shared_ptr <Repo> open(const std::string &path);
-        static std::shared_ptr <Repo> open(git_repository * repo);
-        static std::shared_ptr <Repo> init(const std::string &path, const bool bare = false);
+        ~Repo();
+        static std::shared_ptr < Repo > open(const std::string & path);
+        static std::shared_ptr < Repo > open(git_repository * repo);
+        static std::shared_ptr < Repo > init(const std::string & path,
+                                             const bool bare = false);
 
         // Clone
-        std::shared_ptr <Repo> clone(const std::string &path);
-        static void clone(const std::string &url, const std::string &path);
+         std::shared_ptr < Repo > clone(const std::string & path);
+        static void clone(const std::string & url,
+                          const std::string & path);
 
         // Lookup
-        void lookup(const std::string &commit);
+        void lookup(const std::string & commit);
 
         // Getter
         const std::string path();
 
       private:
-        Repo(Repo &Repo);
-        Repo(const std::string &path);
-        Repo(git_repository * repo);
-         git_repository * repo;
+         Repo(Repo & Repo);
+         Repo(const std::string & path);
+         Repo(git_repository * repo);
+        git_repository *repo;
         static void exception(int error);
-        static git_oid oid(const std::string &sha);
+        static git_oid oid(const std::string & sha);
         static const std::string oid(git_oid & oid);
     };
 }
