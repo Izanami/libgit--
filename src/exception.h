97aa976363549a3b3025bdc488f4548c0c9158ff
Adrien Jeser <adrien@jeser.me>
2013-12-19 23:37:47 +0100

    Gestion des exceptions

---
+++ src/exception.h
@@ -0,0 +1,20 @@
+#ifndef GIT_EXCEPTION_H
+
+#include <git2.h>
+#include <memory>
+#define GIT_EXCEPTION_H
+
+namespace Git {
+    class Exception {
+        public:
+            Exception(const int error);
+            const std::string message();
+
+        private:
+            const git_error *e;
+    };
+
+    void Throw(const int & error);
+};
+
+#endif
--- src/repo.cpp
+++ src/repo.cpp
@@ -1,10 +1,10 @@
+#include "exception.h"
 #include "repo.h"
 
 namespace Git {
     Repo::Repo(const std::string & path) {
         git_threads_init();
-        int error = git_repository_open(&repo, path.c_str());
-         exception(error);
+        Throw(git_repository_open(&repo, path.c_str()));
     } Repo::Repo(git_repository * repo) {
         this->repo = repo;
     }
@@ -12,20 +12,6 @@ namespace Git {
     Repo::~Repo() {
         git_repository_free(this->repo);
     }
-
-    void Repo::exception(int error) {
-        if (error < 0) {
-            const git_error *e = giterr_last();
-
-            if (e != nullptr) {
-                throw e->message;
-            } else {
-                throw "libgit++ : No message error";
-            }
-            giterr_clear();
-        }
-    }
-
 
     std::shared_ptr < Repo > Repo::open(const std::string & path) {
         return std::shared_ptr < Repo > (new Repo(path));
@@ -38,16 +24,13 @@ namespace Git {
     std::shared_ptr < Repo > Repo::init(const std::string & path,
                                         const bool bare) {
         git_repository *repo = nullptr;
-        int error = git_repository_init(&repo, path.c_str(), bare);
-        exception(error);
+        Throw(git_repository_init(&repo, path.c_str(), bare));
         return open(repo);
     }
 
     std::shared_ptr < Repo > Repo::clone(const std::string & path) {
         git_repository *repolone = nullptr;
-        int error =
-            git_clone(&repolone, this->path().c_str(), path.c_str(), NULL);
-        exception(error);
+        Throw(git_clone(&repolone, this->path().c_str(), path.c_str(), NULL));
         return open(repolone);
     }
 
@@ -58,14 +41,12 @@ namespace Git {
     void Repo::lookup(const std::string & commit) {
         git_commit *commit_c;
         git_oid commit_oid = oid(commit.c_str());
-        int error = git_commit_lookup(&commit_c, repo, &commit_oid);
-        exception(error);
+        Throw(git_commit_lookup(&commit_c, repo, &commit_oid));
     }
 
     git_oid Repo::oid(const std::string & sha) {
         git_oid oid;
-        int error = git_oid_fromstr(&oid, sha.c_str());
-        exception(error);
+        Throw(git_oid_fromstr(&oid, sha.c_str()));
         return oid;
     }
     const std::string Repo::oid(git_oid & oid) {
