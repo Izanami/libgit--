7c57b95642417e83295681670b4edb3bb807386b
Adrien Jeser <adrien@jeser.me>
2014-02-18 04:16:47 +0100

    Documentation of src/repo.h

--- src/repo.h
+++ src/repo.h
@@ -1,30 +1,49 @@
 #ifndef GIT_REPO_H
-
 #define GIT_REPO_H
+
+/*!
+ * \file src/repo.h
+ * \brief Manage repository
+ * \author Adrien Jeser <adrien@jeser.me>
+*/
+
 #include <git2.h>
 #include <memory>
 #include "commit.h"
 
+/// \brief libgit++
 namespace Git {
+
+    /// \brief Manage repository
     class Repo:public std::enable_shared_from_this < Repo > {
       public:
         ~Repo();
-        static std::shared_ptr < Repo > open(const std::string & path);
-        static std::shared_ptr < Repo > open(git_repository * repo);
+
+        /// Open
+        static std::shared_ptr < Repo > open(const std::string & path); ///< \param path Path to repository
+        static std::shared_ptr < Repo > open(git_repository * repo); ///< \param repo Repository pointer of libgit2
+
+        /// Initialize a repository
+        /*!
+         * \param path Path to new repository
+         * \param bare Bare repository ?
+         */
         static std::shared_ptr < Repo > init(const std::string & path,
                                              const bool bare = false);
 
-        // Clone
+        /// Clone
          std::shared_ptr < Repo > clone(const std::string & path);
         static void clone(const std::string & url,
                           const std::string & path);
 
         // Getter
-        const std::string path();
-        git_repository *ptr();
+        const std::string path(); ///< Path to repository
+        git_repository *ptr(); ///< Repository pointer of libgit2
 
-        // Commit
+        /// New commit
          std::shared_ptr < Git::Commit > commit();
+
+        /// Get commit
          std::shared_ptr < Git::Commit > commit(const std::string & oid);
          std::shared_ptr < Git::Commit > commit(const git_oid & oid);
 
